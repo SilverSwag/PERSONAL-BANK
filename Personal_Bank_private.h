72da4c7f7f547c2a0148aa96b17e4ba5df6214b1
IanOmwoyo <ian.otuke@strathmore.edu>
2025-03-12 23:37:28 +0800

    commit

--- Personal_Bank_private.h
+++ Personal_Bank_private.h
@@ -5,19 +5,19 @@
 #define PERSONAL_BANK_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"1.0.0.3"
+#define VER_STRING	"1.0.0.4"
 #define VER_MAJOR	1
 #define VER_MINOR	0
 #define VER_RELEASE	0
-#define VER_BUILD	3
+#define VER_BUILD	4
 #define COMPANY_NAME	"SILVERSWAG"
-#define FILE_VERSION	"1.0.0.3"
+#define FILE_VERSION	"1.0.0.4"
 #define FILE_DESCRIPTION	"Developed using the Dev-C++ IDE"
-#define INTERNAL_NAME	""
+#define INTERNAL_NAME	"Personal Bank"
 #define LEGAL_COPYRIGHT	""
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	"Personal Bank"
 #define PRODUCT_NAME	"Personal Bank"
-#define PRODUCT_VERSION	"1.0.0.3"
+#define PRODUCT_VERSION	"1.0.0.4"
 
 #endif /*PERSONAL_BANK_PRIVATE_H*/
