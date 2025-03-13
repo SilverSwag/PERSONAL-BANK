817b120c8b2931511eee2c3ecc85542c0f9e7911
IanOmwoyo <ian.otuke@strathmore.edu>
2025-03-13 17:49:15 +0800

    update

--- Personal Bank.cpp
+++ Personal Bank.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <conio.h>
 #include <iomanip> 
+#include <string>
 using namespace std;
 int security_pin(int pin){
     bool if_running = true;
@@ -19,7 +20,8 @@ int security_pin(int pin){
                 cout<<"Enter [1] to show balance"<<endl;
                 cout<<"Enter [2] to Credit into account"<<endl;
                 cout<<"Enter [3] to Debit from account into ksh account"<<endl;
-                cout<<"Enter [4] to exit"<<endl<<endl;
+                cout<<"Enter [4] to withdraw to M_pessa ccount"<<endl;
+                cout<<"Enter [5] to Exit"<<endl<<endl;
             }else{
                 cout<<"Incorrect Pin!"<<endl;
                 pin_input = 0; // Reset pin_input for the next attempt
@@ -75,9 +77,46 @@ int debit(){
 		cout<<"Current ksh balance: KSH"<<ksh_balance<<endl<<endl;
 	}
 }
+int m_pesa(){
+    string number;
+    float amount;
+    cout << "Enter your phone number: +254 ";
+    cin >> number;
+    if(number.length() != 9 || !isdigit(number[0]) || !isdigit(number[1]) || !isdigit(number[2]) || 
+        !isdigit(number[3]) || !isdigit(number[4]) || !isdigit(number[5]) || 
+        !isdigit(number[6]) || !isdigit(number[7]) || !isdigit(number[8]))
+	{
+        cout<<"Invalid phone number!"<<endl;
+        return 1; // Return an error code
+	}
+    else{
+    	cout<<fixed<<setprecision(2);
+        cout<<"Available balance to withdraw: KSH"<<ksh_balance<<endl;
+        cout<<"Enter amount between ksh100 to ksh250000: KSH";
+        cin>>amount;
+        if(amount <= 0){
+            cout<<"Invalid input!"<<endl;
+            return 1; // Return an error code
+        }else if(amount >= 1 && amount < 100){
+        	cout<<"More than one hundred bob buana!!!!"<<endl;
+        	return 1;
+		}else if(amount > 250000){
+			cout<<"Less than two hundred and fifty thousand bob buana!!!!"<<endl;
+		}
+		else if(amount > ksh_balance){
+            cout<<"Insufficient balance!"<<endl;
+            return 1; // Return an error code
+        }else{
+            ksh_balance -= amount;
+            cout<<"Request to withdraw KSH"<<amount<<" to M-Pesa account +254 "<<number<<" completed"<<endl;
+            cout<<"Current balance: KSH"<<ksh_balance<<endl<<endl;
+            return 0; // Successful operation
+        }
+    }
+}
 int main(int argc, char** argv){
-	cout<<"2025 PERSONAL BANK"<<endl;
-	cout<<"Version 1.1.0"<<endl<<endl;
+	cout<<"Copyright 2025 PERSONAL BANK"<<endl;
+	cout<<"Version 2.0.0"<<endl<<endl;
 	cout<<"ACCOUNT NAME: IAN OMWOYO OTUKE"<<endl;
 	security_pin(5643);
 	int choice;
@@ -92,8 +131,11 @@ int main(int argc, char** argv){
 		}else if(choice == 3){
 			debit();
 		}else if(choice == 4){
+			m_pesa();
+		}else if(choice == 5){
 			if_running = false;
-		}else{
+		}
+		else{
 			cout<<"Option does not exist!!!!!!"<<endl;
 		}
 	}
