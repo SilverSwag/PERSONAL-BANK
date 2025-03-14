5567017679360cc9c21d3855a2fec28facd882c5
IanOmwoyo <ian.otuke@strathmore.edu>
2025-03-14 09:23:52 +0800

    version 3.0.0

--- Personal Bank.cpp
+++ Personal Bank.cpp
@@ -2,11 +2,14 @@
 #include <conio.h>
 #include <iomanip> 
 #include <string>
+#include <algorithm>
+#include <cctype>
 using namespace std;
 int security_pin(int pin){
     bool if_running = true;
     int pin_input = 0;
     int digit;
+    cout<<"Contact the developer for the pin."<<endl;
     cout<<"Enter pin: ";
     while(if_running){
         // Read a character without echoing it
@@ -17,13 +20,14 @@ int security_pin(int pin){
             if(pin_input == pin){
                 if_running = false;
                 cout<<"Select Service"<<endl<<endl;
+                cout<<"(Money should be credited into dollar wallet before it can be converted and deposited into the ksh wallet)"<<endl;
                 cout<<"Enter [1] to show balance"<<endl;
-                cout<<"Enter [2] to Credit into account"<<endl;
-                cout<<"Enter [3] to Debit from account into ksh account"<<endl;
-                cout<<"Enter [4] to withdraw to M_pessa ccount"<<endl;
+                cout<<"Enter [2] to Credit into dollar wallet"<<endl;
+                cout<<"Enter [3] to Debit dollar wallet and credit ksh wallet"<<endl;
+                cout<<"Enter [4] to withdraw to M-pesa account"<<endl;
                 cout<<"Enter [5] to Exit"<<endl<<endl;
             }else{
-                cout<<"Incorrect Pin!"<<endl;
+                cout<<"ERROR! Incorrect Pin!"<<endl;
                 pin_input = 0; // Reset pin_input for the next attempt
                 cout<<"Enter pin: "; // Prompt again
             }
@@ -34,90 +38,89 @@ int security_pin(int pin){
     }
     return 0; // Return 0 or any other value as needed
 }
-float available_balance = 1000;
-float dollar_balance = 876.87;
-float ksh_balance = 54789.65;
-float exchange_rate = 133.89;
+float available_balance = 7500;
+float dollar_balance = 0.00;
+float ksh_balance = 0.00;
+float exchange_rate = 129;
 int showbalance(){
 	cout<<fixed<<setprecision(2);
 	cout<<"Available balance: $"<<available_balance<<endl;
-	cout<<"Dollar balance: $"<<dollar_balance<<endl;
-	cout<<"Ksh Balance: KSH"<<ksh_balance<<endl<<endl;
+	cout<<"Dollar wallet balance: $"<<dollar_balance<<endl;
+	cout<<"Ksh wallet balance: KSH"<<ksh_balance<<endl<<endl;
 }
 int credit(){
 	cout<<fixed<<setprecision(2);
 	float credit_amount;
-	cout<<"Enter amount between $1.00 to $"<<available_balance<<" to Credit into account: ";
+	cout<<"Enter amount between $1.00 to $"<<available_balance<<" to credit dollar wallet: ";
 	cin>>credit_amount;
 	if(credit_amount > available_balance){
-		cout<<"Insufficient balance!"<<endl;
+		cout<<"ERROR! Insufficient funds!"<<endl;
 	}else if(credit_amount <= 0){
-		cout<<"Invalid input!"<<endl;
+		cout<<"ERROR! Invalid input!"<<endl;
 	}else{
 		dollar_balance += credit_amount;
 		available_balance -= credit_amount;
-		cout<<"Request to credit $"<<credit_amount<<" completed"<<endl;
-		cout<<"Current dollar balance: $"<<dollar_balance<<endl<<endl;
+		cout<<"Request to credit $"<<credit_amount<<" into dollar wallet completed"<<endl;
+		cout<<"Current dollar wallet balance: $"<<dollar_balance<<endl<<endl;
 	}
 }
 int debit(){
 	cout<<fixed<<setprecision(2);
 	float debit_amount;
-	cout<<"Enter amount between $1.00 to $"<<dollar_balance<<" to credit ksh account: ";
+	cout<<"Enter amount between $1.00 to $"<<dollar_balance<<" to credit ksh wallet: ";
 	cin>>debit_amount;
 	if(debit_amount > dollar_balance){
-		cout<<"Insufficient balance!"<<endl;
+		cout<<"ERROR! Insufficient funds!"<<endl;
 	}else if(debit_amount <= 0){
-		cout<<"Invalid input!"<<endl;
+		cout<<"ERROR! Invalid input!"<<endl;
 	}else{
 		ksh_balance += (debit_amount * exchange_rate);
 		dollar_balance -= debit_amount;
-		cout<<"Request to credit $"<<debit_amount<<" into ksh account completed"<<endl;
-		cout<<"Exchange rate: $1 = KSH"<<exchange_rate<<endl;
-		cout<<"Current ksh balance: KSH"<<ksh_balance<<endl<<endl;
+		cout<<"Request to credit $"<<debit_amount<<" into ksh wallet completed"<<endl;
+		cout<<"Exchange rate: $1.00 = KSH"<<exchange_rate<<endl;
+		cout<<"Current ksh wallet balance: KSH"<<ksh_balance<<endl<<endl;
 	}
 }
 int m_pesa(){
     string number;
     float amount;
-    cout << "Enter your phone number: +254 ";
-    cin >> number;
+    cout<<"Enter your phone number: +254 ";
+    cin>>number;
     if(number.length() != 9 || !isdigit(number[0]) || !isdigit(number[1]) || !isdigit(number[2]) || 
         !isdigit(number[3]) || !isdigit(number[4]) || !isdigit(number[5]) || 
         !isdigit(number[6]) || !isdigit(number[7]) || !isdigit(number[8]))
 	{
-        cout<<"Invalid phone number!"<<endl;
-        return 1; // Return an error code
+        cout<<"ERROR! Invalid phone number!"<<endl;
 	}
     else{
     	cout<<fixed<<setprecision(2);
         cout<<"Available balance to withdraw: KSH"<<ksh_balance<<endl;
-        cout<<"Enter amount between ksh100 to ksh250000: KSH";
+        cout<<"Enter amount between ksh100.00 to ksh250,000.00: KSH";
         cin>>amount;
         if(amount <= 0){
-            cout<<"Invalid input!"<<endl;
-            return 1; // Return an error code
+            cout<<"ERROR! Invalid input!"<<endl;
         }else if(amount >= 1 && amount < 100){
-        	cout<<"More than one hundred bob buana!!!!"<<endl;
-        	return 1;
+        	cout<<"ERROR! Minimum amount to withdraw is ksh100.00"<<endl;
 		}else if(amount > 250000){
-			cout<<"Less than two hundred and fifty thousand bob buana!!!!"<<endl;
+			cout<<"ERROR! Maximum amount to withdraw is ksh250,000.00"<<endl;
 		}
 		else if(amount > ksh_balance){
-            cout<<"Insufficient balance!"<<endl;
-            return 1; // Return an error code
+            cout<<"ERROR! Insufficient funds!"<<endl;
         }else{
             ksh_balance -= amount;
             cout<<"Request to withdraw KSH"<<amount<<" to M-Pesa account +254 "<<number<<" completed"<<endl;
-            cout<<"Current balance: KSH"<<ksh_balance<<endl<<endl;
-            return 0; // Successful operation
+            cout<<"Current ksh wallet balance: KSH"<<ksh_balance<<endl<<endl;
         }
     }
 }
 int main(int argc, char** argv){
-	cout<<"Copyright 2025 PERSONAL BANK"<<endl;
-	cout<<"Version 2.0.0"<<endl<<endl;
-	cout<<"ACCOUNT NAME: IAN OMWOYO OTUKE"<<endl;
+	cout<<"Copyright 2025 PERSONAL BANK: Financial benefits"<<endl;
+	cout<<"Version 3.0.0"<<endl<<endl;
+	string name;
+	cout<<"Enter your name: ";
+	getline(cin, name);
+	transform(name.begin(), name.end(), name.begin(), ::toupper);
+	cout<<"ACCOUNT NAME: "<<name<<endl;
 	security_pin(5643);
 	int choice;
 	bool if_running = true;
@@ -136,7 +139,7 @@ int main(int argc, char** argv){
 			if_running = false;
 		}
 		else{
-			cout<<"Option does not exist!!!!!!"<<endl;
+			cout<<"ERROR! Invalid option!"<<endl;
 		}
 	}
 	return 0;
--- Personal_Bank_private.h
+++ Personal_Bank_private.h
@@ -5,19 +5,19 @@
 #define PERSONAL_BANK_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"2.0.0.23"
-#define VER_MAJOR	2
+#define VER_STRING	"3.0.0.2"
+#define VER_MAJOR	3
 #define VER_MINOR	0
 #define VER_RELEASE	0
-#define VER_BUILD	23
+#define VER_BUILD	2
 #define COMPANY_NAME	"SILVERSWAG"
-#define FILE_VERSION	"2.0.0.23"
+#define FILE_VERSION	"3.0.0.2"
 #define FILE_DESCRIPTION	"Developed by Ian Omwoyo"
 #define INTERNAL_NAME	"Personal Bank"
 #define LEGAL_COPYRIGHT	"©2025 Personal Bank"
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	"Personal Bank"
 #define PRODUCT_NAME	"Personal Bank"
-#define PRODUCT_VERSION	"2.0.0.23"
+#define PRODUCT_VERSION	"3.0.0.2"
 
 #endif /*PERSONAL_BANK_PRIVATE_H*/
